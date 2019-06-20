0531285e1133994f499d57710b6f981f3889eaf0
Anas MESRI <anas.mesri.dz@viacesi.fr>
2019-06-20 20:29:56 +0100

    Batman! (this commit has no parents)

---
+++ world/src/main/java/world/elements/block/Dirt.java
@@ -0,0 +1,36 @@
+package world.elements.block;
+
+import world.Position;
+import world.elements.SpriteManager;
+import world.elements.entity.Character;
+import world.reaction.Remove;
+import world.reaction.Sides;
+
+import java.awt.*;
+
+/**
+ * Dirt Element
+ * @see Block
+ * */
+public class Dirt extends Block{
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 6377188566631574195L;
+
+	public Dirt(Position position, Dimension dimension){
+        super(position, dimension, new SpriteManager(
+                "Dirt.png"
+        ));
+
+        Sides[] sides = {Sides.LEFT, Sides.RIGHT, Sides.BOTTOM, Sides.TOP};
+        this.reactions.add(new Remove(this, Character.class, sides,1));
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        this.engine.playSound("Dig");
+    }
+}
